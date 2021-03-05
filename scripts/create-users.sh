207dde83394e7b82b0cce0f7076c845623a34615
Ryan Cox <ryan.cox@salesforce.com>
2021-03-05 00:46:27 -0700

    setup

--- scripts/create-users.sh
+++ scripts/create-users.sh
@@ -1,3 +1,4 @@
 USER_ALIAS="${USER_ALIAS:-sfdx-employee-title-change__dev}"
 echo creating user w/ USER_ALIAS = $USER_ALIAS
 sfdx force:user:create -u $USER_ALIAS -f ../users/employee-def.json 
+sfdx force:user:create -u $USER_ALIAS -f ../users/employee-def.json 
